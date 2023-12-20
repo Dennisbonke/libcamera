4460fd8dafeb93b49a13cac78b2f3d167bb77462
Dennis Bonke <admin@dennisbonke.com>
2023-12-20 14:35:40 +0100

    libcamera/software_isp: Document the SwStats class
    Signed-off-by: Dennis Bonke <admin@dennisbonke.com>

--- include/libcamera/internal/software_isp/swstats.h
+++ include/libcamera/internal/software_isp/swstats.h
@@ -26,55 +26,115 @@ struct StreamConfiguration;
 
 LOG_DECLARE_CATEGORY(SwStats)
 
+/**
+ * \class SwStats
+ * \brief Base class for the software ISP statistics.
+ *
+ * Base class for the software ISP statistics.
+ */
 class SwStats
 {
 public:
 	virtual ~SwStats() = 0;
 
+	/**
+	 * \brief Gets wether the statistics object is valid.
+	 * 
+	 * \return true if it's valid, false otherwise.
+	 */
 	virtual bool isValid() const = 0;
 
-	/*
-	 * Setup the SwStats object for the passed in input format.
-	 * Return 0 on success, a negative errno value on failure
-	 * (unsupported format or failed to create the SharedMemObject).
+	/**
+	 * \brief Configure the statistics object for the passed in input format.
+	 * \param[in] inputCfg The input format
+	 *
+	 * \return 0 on success, a negative errno value on failure.
 	 */
 	virtual int configure(const StreamConfiguration &inputCfg) = 0;
 
-	/* Get the FD for the SharedMemObject for the stats. */
+	/**
+	 * \brief Get the file descriptor for the statistics.
+	 *
+	 * \return the file descriptor
+	 */
 	virtual const SharedFD &getStatsFD() = 0;
 
 protected:
+	/**
+	 * \brief Called when there is data to get statistics from.
+	 * \param[in] src The input data.
+	 * \param[in] stride The stride
+	 */
 	typedef void (SwStats::*statsProcessFn)(const uint8_t *src, unsigned int stride);
+	/**
+	 * \brief Called when the statistics gathering is done or when a new frame starts.
+	 */
 	typedef void (SwStats::*statsVoidFn)();
 
 	/* Variables set by configure(), used every line */
+	/**
+	 * \brief The function called when a line is ready for statistics processing.
+	 *
+	 * Used for line 0 and 1, repeating if there isn't a 3rd and a 4th line in the bayer order.
+	 */
 	statsProcessFn stats0_;
+	/**
+	 * \brief The function called when a line is ready for statistics processing.
+	 *
+	 * Used for line 3 and 4, only needed if the bayer order has 4 different lines.
+	 */
 	statsProcessFn stats2_;
 
+	/**
+	 * \brief The memory used per pixel.
+	 */
 	unsigned int bpp_;         /* Memory used per pixel, not precision */
+	/**
+	 * \brief Skip lines where this bitmask is set in y.
+	 */
 	unsigned int y_skip_mask_; /* Skip lines where this bitmask is set in y */
 
-	/* Statistics windows, set by setWindow(), used every line */
+	/**
+	 * \brief Statistics window, set by setWindow(), used ever line.
+	 */
 	Rectangle window_;
 
-	/* Variables set by configure(), used once per frame */
+	/**
+	 * \brief The function called at the start of a frame.
+	 */
 	statsVoidFn startFrame_;
+	/**
+	 * \brief The function called at the end of a frame.
+	 */
 	statsVoidFn finishFrame_;
+	/**
+	 * \brief The size of the bayer pattern.
+	 */
 	Size patternSize_;
+	/**
+	 * \brief The offset of x, applied to window_.x for bayer variants.
+	 *
+	 * This can either be 0 or 1.
+	 */
 	unsigned int x_shift_; /* Offset of 0/1 applied to window_.x for bayer variants */
 
 public:
-	/*
+	/**
+	 * \brief Get the pattern size.
+	 *
 	 * For some input-formats, e.g. Bayer data, processing is done multiple lines
 	 * and/or columns at a time. Get width and height at which the (bayer) pattern
 	 * repeats. Window values are rounded down to a multiple of this and the height
 	 * also indicates if processLine2() should be called or not.
 	 * This may only be called after a successful configure() call.
+	 *
+	 * \return the pattern size.
 	 */
 	const Size &patternSize() { return patternSize_; }
 
-	/*
-	 * Specify window coordinates over which to gather statistics.
+	/**
+	 * \brief Specify window coordinates over which to gather statistics.
+	 * \param[in] window The window object.
 	 */
 	void setWindow(Rectangle window)
 	{
@@ -90,8 +150,9 @@ public:
 		window_.height &= ~(patternSize_.height - 1);
 	}
 
-	/*
-	 * Reset state to start statistic gathering for a new frame.
+	/**
+	 * \brief Reset state to start statistics gathering for a new frame.
+	 * 
 	 * This may only be called after a successful setWindow() call.
 	 */
 	void startFrame()
@@ -99,10 +160,15 @@ public:
 		(this->*startFrame_)();
 	}
 
-	/*
-	 * Process line 0 of input formats with patternSize height == 1.
-	 * Process line 0 + 1 of input formats with patternSize height >= 2.
-	 * This may only be called after a successful setWindow() call.
+	/**
+	 * \brief Process line 0.
+	 * \param[in] y The y coordinate.
+	 * \param[in] src The input data.
+	 * \param[in] stride The stride.
+	 *
+	 * This function processes line 0 for input formats with patternSize height == 1.
+	 * It'll process line 0 and 1 for input formats with patternSize height >= 2.
+	 * This function may only be called after a successful setWindow() call.
 	 */
 	void processLine0(unsigned int y, const uint8_t *src, unsigned int stride)
 	{
@@ -113,9 +179,14 @@ public:
 		(this->*stats0_)(src + window_.x * bpp_ / 8, stride);
 	}
 
-	/*
-	 * Process lines 2 + 3 of input formats with patternSize height == 4.
-	 * This may only be called after a successful setWindow() call.
+	/**
+	 * \brief Process line 2 and 3.
+	 * \param[in] y The y coordinate.
+	 * \param[in] src The input data.
+	 * \param[in] stride The stride.
+	 *
+	 * This function processes line 2 and 3 for input formats with patternSize height == 4.
+	 * This function may only be called after a successful setWindow() call.
 	 */
 	void processLine2(unsigned int y, const uint8_t *src, unsigned int stride)
 	{
@@ -126,9 +197,9 @@ public:
 		(this->*stats2_)(src + window_.x * bpp_ / 8, stride);
 	}
 
-	/*
-	 * Finish statistics calculation for current frame and
-	 * store the results in the SharedMemObject.
+	/**
+	 * \brief Finish statistics calculation for the current frame.
+	 * 
 	 * This may only be called after a successful setWindow() call.
 	 */
 	void finishFrame()
@@ -136,14 +207,22 @@ public:
 		(this->*finishFrame_)();
 	}
 
-	/*
-	 * Process a whole frame at once, replacing manually calling
-	 * startFrame() + processLine0() + finishFrame().
+	/**
+	 * \brief Process a whole frame at once.
+	 * \param[in] src The input data.
+	 * \param[in] stride The stride.
+	 *
+	 * This function replaces manually calling startFrame()
+	 * + processLine0() + finishFrame().
 	 * This may only be called after a successful setWindow() call.
 	 */
 	void processFrame(const uint8_t *src, unsigned int stride);
 
-	/* The int parameter isn't actually used */
+	/**
+	 * \brief Signals that the statistics are ready.
+	 *
+	 * The int parameter isn't actually used.
+	 */
 	Signal<int> statsReady;
 };
 
